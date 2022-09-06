82125de3a0c8e2c185c143cdbc0595db89622c4f
Arman <keremeris20@gmail.com>
2022-09-06 12:29:30 +0400

    ss

--- js/updateImages.js
+++ js/updateImages.js
@@ -4,7 +4,7 @@
 
 //server api
 const API = "https://imagesetting.vercel.app/api/img";
-
+const API_info = "https://imagesetting.vercel.app/info/img";
 function updateImg(imgID) {
   const image = images[imgID];
 
@@ -19,6 +19,29 @@ function updateImg(imgID) {
         console.log(path, downloadURL);
         //saving the image link in the next js mongodb
         fetch(API + "/", {
+          method: "POST",
+          body: JSON.stringify({
+            path,
+            link: downloadURL,
+          }),
+        }).then((res) => console.log(res.body));
+      });
+    });
+}
+function updateImg_info(imgID) {
+  const image = images[imgID];
+
+  const path = imgID;
+  var storageRef = firebase.storage().ref();
+  storageRef
+    .child(path)
+    .put(image)
+    .then((snapshot) => {
+      console.log("Uploaded a blob or file!", snapshot);
+      snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        console.log(path, downloadURL);
+        //saving the image link in the next js mongodb
+        fetch(API_info + "/", {
           method: "POST",
           body: JSON.stringify({
             path,
